113c4a9fcc4a997455b6def074e4bde3fa8491e4
Your Name <you@example.com>
2020-05-15 10:26:51 -0500

    Added new plot, updated old plots, added spellcheck example, added plotting script

--- plot_results.py
+++ plot_results.py
@@ -13,6 +13,7 @@ def main():
 	cleanColums(df)
 	cleanNetworkType(df)
 	plotColumns(df)
+	plotNormedRuntime(df)
 
 def cleanColums(df):
 	tmp = df.columns
@@ -23,27 +24,44 @@ def cleanNetworkType(df):
 	tmp = [('shallow' if 'Shallow' in bla else 'medium') for bla in df['NetworkType']]
 	df['NetworkType'] = tmp
 
+def plotNormedRuntime(df):
+	idx = 'CleaningIndex'
+	runtime = 'Runtime'
+	epochs = 'MinEpoch'
+	shallow = df.loc[df['NetworkType']=='shallow']
+	medium = df.loc[df['NetworkType']=='medium']
+	mpp.plot(shallow[idx],shallow[runtime]/shallow[epochs],c=shallowCol,marker='x',label='Shallow Network')
+	mpp.plot(medium[idx],medium[runtime]/medium[epochs],c=mediumCol,marker='.',label='Medium Network')
+	mpp.title('Run time per epoch over data cleaning')
+	mpp.ylabel('Run time per epoch [s]')
+	#mpp.ylim()
+	mpp.savefig('images/plot_timePerEpoch.png')
+
 def plotColumns(df, idx='CleaningIndex', cols=[ 'TestAccuracy', 'MinEpoch', 'MinValidationLoss', 'Runtime']):
 	shallow = df.loc[df['NetworkType']=='shallow']
 	medium = df.loc[df['NetworkType']=='medium']
 	for col in cols:
 		mpp.plot(shallow[idx],shallow[col],c=shallowCol,marker='x',label='Shallow Network')
 		mpp.plot(medium[idx],medium[col],c=mediumCol,marker='.',label='Medium Network')
-		mpp.title('Dependence of {} on data cleaning'.format(col))
 		mpp.xlabel('Cleaning level')
 		mpp.legend()
-		if col == 'RunTime':
-			mpp.ylabel('{} [s]'.format(col))
+		if col == 'Runtime':
+			mpp.title('Dependence of run time on data cleaning')
+			mpp.ylabel('Run time [s]')
 			#mpp.ylim()
 		elif col == 'TestAccuracy':
-			mpp.ylabel('{} [%]'.format(col))
+			mpp.title('Dependence of test accuracy on data cleaning')
+			mpp.ylabel('Test accuracy')
 			mpp.ylim(0.5,1)
 		elif col == 'MinEpoch':
-			mpp.ylabel(col)
+			mpp.title('Dependence of optimal training epoch on data cleaning')
+			mpp.ylabel('Optimal training epoch')
 			mpp.ylim(0,100)
 		elif col == 'MinValidationLoss':
-			mpp.ylabel('{} [%]'.format(col))
-		mpp.savefig('plot_{}.png'.format(col))
+			mpp.title('Dependence of minimal validation loss on data cleaning')
+			mpp.ylabel('Validation loss at optimal epoch')
+			mpp.ylim(.18,.68)
+		mpp.savefig('images/plot_{}.png'.format(col))
 		mpp.close()
 
 if __name__ == '__main__':
